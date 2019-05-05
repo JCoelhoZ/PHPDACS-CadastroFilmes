8448d4a74221824aab4344c2e0f531ff3d350d72
Jaqueline Zacher <jaquezacher@gmail.com>
2019-05-05 16:03:31 -0300

    Updates

--- filme.php
+++ filme.php
@@ -1,7 +1,7 @@
 <?php
-	$con = mysqli_connect("localhost", "root", "univille", "locadora");
+	$con = mysqli_connect("localhost", "bob", "bob", "dacs2019php");
 $stmt = "INSERT INTO filme(idfilme, nome, duracao) VALUES (?,?,?)";
 $stmtprep = mysqli_prepare($con, $stmt);
-mysqli_stmt_bind_param($stmtprep, "isi", $_POST["txtid"], $_POST["txtnome"], $_POST["txttempo"] );
+mysqli_stmt_bind_param($stmtprep, "isi", $_POST["txtid"], $_POST["txtnome"], $_POST["txtduracao"] );
 mysqli_stmt_execute($stmtprep);
 ?>
